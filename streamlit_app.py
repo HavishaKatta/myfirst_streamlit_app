64583e37649c3060683b530dd1f541e0ecb521eb
HavishaKatta <140058778+HavishaKatta@users.noreply.github.com>
2023-07-26 13:51:53 +0530

    Update streamlit_app.py
    display selected fruits to show

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,10 @@ streamlit.dataframe(my_fruit_list)
 # Adding Picklist
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
+# Pick fruits to be included
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+# display table on top page
+streamlit.dataframe(fruits_to_show)
 
